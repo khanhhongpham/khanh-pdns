865341d3064f13ae1e7b84a0a288693d7d32b0c2
k4pham <k4pham@gmail.com>
2018-01-15 11:08:40 +0700

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/recipes/security.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: security
+#
+# Copyright:: 2018, Khanh Pham Hong, All Rights Reserved.
+include_recipe 'delivery-truck::security'
---
+++ .delivery/build_cookbook/recipes/syntax.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: syntax
+#
+# Copyright:: 2018, Khanh Pham Hong, All Rights Reserved.
+include_recipe 'delivery-truck::syntax'
